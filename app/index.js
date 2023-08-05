ad81fcae167181ae2bb014ba04467e3472dc7477
fseda <felipeseda@poli.ufrj.br>
2023-08-04 21:27:12 -0300

    Add redis client

--- app/index.js
+++ app/index.js
@@ -3,21 +3,20 @@ const redis = require('redis');
 const process = require('process');
 
 const app = express();
-// const client = redis.createClient({
-//   host: 'redis-server',
-//   port: 6379,
-// });
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379,
+});
 
-// client.set('visits', 0);
+client.set('visits', 0);
 
 app.get('/', (req, res) => {
   // process.exit(0);
-  // client.get('visits', (err, visits) => {
-  //   res.send('Number of visits ' + visits);
-  //   client.set('visits', parseInt(visits) + 1);
-  // });
+  client.get('visits', (err, visits) => {
+    res.send('Number of visits ' + visits);
+    client.set('visits', parseInt(visits) + 1);
+  });
 
-  res.send('world hello!');
 });
 
 const port = 8080;
